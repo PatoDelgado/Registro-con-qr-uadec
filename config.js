09f5f2abc5aa262782a47272aa85697d8b891b72
Patricio Delgado <patotrejo@MacBook-Pro.local>
2026-07-17 02:39:21 -0600

    Config.js corregido

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.API_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
   ? "http://localhost:3000/api"
-  : "https://registro-visitas-a-empresas.onrender.com/api";
+  : `${window.location.origin}/api`;
